dfff28629fd0f6d3735ce9c276de0785dc66ebc0
Mohamed Diaa <mohamedxdiaa@gmail.com>
2025-05-20 15:30:20 +0300

    + Display Products

--- script.js
+++ script.js
@@ -43,10 +43,30 @@ function addProduct() {
     }
 }
 function clearInputs() {
-    productCode.value = "";
-    productName.value = "";
-    productPrice.value = "";
-    productCondition.value = "";
-    productImage.value = "";
+  productCode.value = "";
+  productName.value = "";
+  productPrice.value = "";
+  productCondition.value = "";
+  productImage.value = "";
+}
+
+function displayProduct(arr) {
+    let cartoona = ``;
+    for (let i = 0; i < arr.length; i++) {
+      cartoona += `<div class="col-3">
+                  <div class="card shadow bg-newLight border-2 border-blue mt-2 p-3">
+                      <img class="img-fluid" src=" ${arr[i].image}" alt="Mobile Phone">
+                      <h6 class="text-dark">Code: ${arr[i].code}</h6>
+                      <h6 class="text-dark">Name: ${arr[i].Name}</h6>
+                      <h6 class="text-dark">Price: ${arr[i].price}</h6>
+                      <h6 class="text-dark">Condition: ${arr[i].condition}</h6>
+                      <div class="d-flex gap-2">
+                          <button onclick="editProduct(${i})" class="btn btn-outline-warning w-50">Edit <i class="bi bi-pencil"></i></button>
+                          <button onclick="deleteProduct(${i})" class="btn btn-outline-danger w-50">Delete <i class="bi bi-trash"></i></button>                        
+                          </div>
+                          </div>
+              </div>`;
+    }
+    productList.innerHTML = cartoona;
   }
   
