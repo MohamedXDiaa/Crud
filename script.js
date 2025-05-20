96fe0fd24f92f826dd94d63165dc330c10672fc0
Mohamed Diaa <mohamedxdiaa@gmail.com>
2025-05-20 15:19:58 +0300

    + Product Container

--- script.js
+++ script.js
@@ -8,3 +8,45 @@ let productList = document.getElementById("productList");
 let searchInput = document.getElementById("searchInput");
 let productImage = document.getElementById("imageInput");
 
+let productContainer = [];
+if (localStorage.getItem("product") !== null) {
+  productContainer = JSON.parse(localStorage.getItem("product"));
+  displayProduct(productContainer);
+}
+
+function addProduct() {
+    let product = {
+      code: productCode.value,
+      Name: productName.value,
+      price: productPrice.value,
+      condition: productCondition.value,
+      image: "",
+    };
+    let file = productImage.files[0];
+    if (file) {
+      let reader = new FileReader();
+      reader.onload = function () {
+        product.image = reader.result;
+        productContainer.push(product);
+        localStorage.setItem("product", JSON.stringify(productContainer));
+        displayProduct(productContainer);
+        clearInputs();
+      };
+      reader.readAsDataURL(file);
+    }
+    else {
+      product.image = "https://static.vecteezy.com/system/resources/previews/005/720/408/original/crossed-image-icon-picture-not-available-delete-picture-symbol-free-vector.jpg";
+      productContainer.push(product);
+      localStorage.setItem("product", JSON.stringify(productContainer));
+      displayProduct(productContainer);
+      clearInputs();
+    }
+}
+function clearInputs() {
+    productCode.value = "";
+    productName.value = "";
+    productPrice.value = "";
+    productCondition.value = "";
+    productImage.value = "";
+  }
+  
