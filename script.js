fb3ef2b995ecf1616ac73db1df2cd42c3697a548
Mohamed Diaa <mohamedxdiaa@gmail.com>
2025-05-20 15:17:38 +0300

    Get Element by ids on java

---
+++ script.js
@@ -0,0 +1,10 @@
+let productCode = document.getElementById("productCode");
+let productName = document.getElementById("productName");
+let productPrice = document.getElementById("productPrice");
+let productCondition = document.getElementById("productCondition");
+let addBtn = document.getElementById("btnAdd");
+let editBtn = document.getElementById("btnEdit");
+let productList = document.getElementById("productList");
+let searchInput = document.getElementById("searchInput");
+let productImage = document.getElementById("imageInput");
+
